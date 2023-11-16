f0ecf201f4d7a2c0b96f18c1e1d7406f3c0a3a04
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-11-16 11:05:20 +0700

    1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,13 @@
 //https://www.pepsi.com/en-us/refresh082123/media/PEPSI_2023_PR_Sizzle_1920px_trimmed.mp4
 //https://www.pepsi.com/en-us/refresh082123/media/PEPSI_2023_PR_Sizzle_1920px_trimmed.mp4
-const navigation = document.querySelector('#footer')
-   navigation.innerHTML=
+
+   
+
+const footer = document.querySelector('#footer')
+   footer.innerHTML=
    `
-   <div class='footerContainer container-fluid row d-flex align-items-center' style="height:200px;color:white; background:#0025ff">
-       <div class='logo col-xl-2 d-flex align-items-center'>
+   <div class='footerContainer  row d-flex align-items-center' style="height:200px;color:white; background:#0025ff;width:100%;margin:0">
+       <div class='logo col-xl-2 d-flex align-items-center' >
          <img src='image/logo.png'/>
        </div>
        <div class=' col-xl-2 d-flex align-items-center flex-column justify-content-center'>
@@ -28,5 +31,22 @@ const navigation = document.querySelector('#footer')
        <i class='bx bxl-youtube display-4 mx-xl-4' ></i>
        <i class='bx bxl-tiktok display-4 mx-xl-4' ></i>
        </div>
+   </div>
+   `
+  
+   
+   slider.innerHTML=
+   `
+   <div class='sliderContainer container-fluid d-flex flex-column align-items-center mt-lg-4 ' style="background:#000 ;color:#1636ff">
+     <h1 class='display-5 my-lg-4' style=" font-family: 'Roboto',sans-serif; font-weight:bold">STAY ON THE PULSE. FOLLOW PEPSI.</h1>
+     <div class=' imgList row mb-lg-5 ' style='height:auto ;  width:90% ;border:3px solid #1636ff' >
+    
+   <div class="col-lg-2 imgItems">   <img  src= "image/background2.png"/></div>
+   <div class="col-lg-2 imgItems">   <img  src= "image/background3.png"/></div>
+   <div class="col-lg-2 imgItems">   <img  src= "image/background4.png"/></div>
+   <div class="col-lg-2 imgItems">   <img  src= "image/background5.png"/></div>
+   <div class="col-lg-2 imgItems">   <img  src= "image/background6.png"/></div>
+   <div class="col-lg-2 imgItems">   <img  src= "image/backgroung7.png"/></div>
+     </div>
    </div>
    `

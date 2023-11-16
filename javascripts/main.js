25efb45496311c14fd92686ec11acdb5de59bc9b
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-11-16 09:40:40 +0700

    1

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,30 @@
 const navigation = document.querySelector('#footer')
    navigation.innerHTML=
    `
-   <div class='footerContainer container-fluid'>
-       <div class='logo col-sm-3'>
+   <div class='footerContainer container-fluid row d-flex align-items-center' style="height:300px;color:white; background:#0025ff">
+       <div class='logo col-xl-2 d-flex align-items-center'>
+         <img src='image/logo.png'/>
+       </div>
+       <div class=' col-xl-2 d-flex align-items-center flex-column justify-content-center'>
+         <p> CONTACT US </p>
+         <p>TERMS & CONDITIONS</p>
+         <p>PRIVACY POLICY</p>
+       </div>
+       <div class='col-xl-2 d-flex align-items-center flex-column justify-content-center'>
+        <p>COPYRIGHT</p>
+        <p>PRODUCT FACTS</p>
+        <p> STORE LOCATOR</p>
+       </div>
+       <div class='col-xl-2 d-flex align-items-center flex-column justify-content-center'>
+       <p>SERVE PEPSI</p>
+       <p>ABOUT OUR ADS</p>
+       <p> CAREERS</p>
+       </div>
+       <div class='col-xl-2 d-flex align-items-center justify-content-center mx-sm-5'>
+       <i class='bx bxl-instagram display-4 mx-xl-4'></i>
+       <i class='bx bxl-facebook display-4 mx-xl-4' ></i>
+       <i class='bx bxl-youtube display-4 mx-xl-4' ></i>
+       <i class='bx bxl-tiktok display-4 mx-xl-4' ></i>
        </div>
    </div>
    `
